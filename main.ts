a74f81340c8816f7af26ab6cd21b47382ba8a1ba
Farid Mohd Ismail <farid.skyman@gmail.com>
2021-12-08 21:29:00 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+devices.onGamepadButton(MesDpadButtonInfo.BDown, function () {
+    basic.showArrow(ArrowNames.South)
+})
+devices.onGamepadButton(MesDpadButtonInfo._2Down, function () {
+    basic.showArrow(ArrowNames.South)
+})
+devices.onGamepadButton(MesDpadButtonInfo.DDown, function () {
+    basic.showArrow(ArrowNames.West)
+})
+devices.onGamepadButton(MesDpadButtonInfo.CDown, function () {
+    basic.showArrow(ArrowNames.East)
+})
+devices.onGamepadButton(MesDpadButtonInfo.ADown, function () {
+    basic.showArrow(ArrowNames.North)
+})
+devices.onGamepadButton(MesDpadButtonInfo._1Up, function () {
+    basic.showArrow(ArrowNames.North)
+})
+basic.forever(function () {
+	
+})
